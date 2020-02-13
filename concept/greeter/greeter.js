603745cf0a12c64d9ac2878fbfcdf77e40f6f8b4
Niklas Kreer <kreergit@posteo.de>
2020-02-13 08:12:16 +0100

    Focus on the destination field again after a via station has been added

--- concept/greeter/greeter.js
+++ concept/greeter/greeter.js
@@ -2,29 +2,26 @@
 var viaStationCount = 0;
 
 function spawnViaStation(){
-    if(viaStationCount === 0){
-        var via1 = document.getElementById("via1");
-        var via1text = document.getElementById("via1text");
-        via1.className = "";
-        // Move text from destination to via1
-        destination = document.getElementById("destination");
-        via1text.value = destination.value;
+    destination = document.getElementById("destination");
+    
+    if(viaStationCount < 2){
+        nextViaCount = viaStationCount + 1;
+        var via = document.getElementById("via" + nextViaCount);
+        var viatext = document.getElementById("via" + nextViaCount + "text");
+        via.className = "";
+        // Move text from destination to new via box
+        viatext.value = destination.value;
         destination.value = "";
         viaStationCount++;
-    } else if(viaStationCount === 1){
-        var via2 = document.getElementById("via2");
-        var via2text = document.getElementById("via2text");
-        via2.className = "";
-        // Move text from destination to via2
-        destination = document.getElementById("destination");
-        via2text.value = destination.value;
-        destination.value = "";
-        // Hide the via button
+    }
+
+    // Hide the via button when the maximum is reached
+    if(viaStationCount === 2){
         button = document.getElementById("addVia");
         button.className = "hiddenElement";
         button.parentNode.style.display = "block";
-        viaStationCount++;
-    } else {
-        alert("There may only be two via stations");
     }
+
+    // Focus back on the destination field for immediate next entry
+    destination.focus();
 }
