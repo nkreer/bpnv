9ea1211fa0d6494c8219c06deb98cc625bb54b83
Niklas Kreer <kreergit@posteo.de>
2020-02-13 08:22:35 +0100

    Keyboard use of entire procedure in greeter mockup

--- concept/greeter/greeter.js
+++ concept/greeter/greeter.js
@@ -25,3 +25,18 @@ function spawnViaStation(){
     // Focus back on the destination field for immediate next entry
     destination.focus();
 }
+
+document.addEventListener("keydown", function(event){
+    var destination = document.getElementById("destination");
+    // Only do the magic when the destination box is active
+    if(destination === document.activeElement){
+        // Spawn a new via station when the user presses enter and shift at the same time
+        if(event.shiftKey && event.keyCode === 13){
+            spawnViaStation();
+        } else if
+        // Continue with a search when enter is pressed
+        (event.keyCode === 13){
+            alert("Commencing search");
+        }
+    }
+});
